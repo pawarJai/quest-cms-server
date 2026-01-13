6fed5ef21647c8c2e4423fbc564d14354442e511
pawarJai <pawarjayesh468@gmail.com>
2026-01-13 16:35:29 +0530

    working on contact us

--- app/repository/quote_repository.py
+++ app/repository/quote_repository.py
@@ -8,16 +8,16 @@ COLLECTION = db["quotes"]
 class QuoteRepository:
 
     @staticmethod
-    async def create_quote(data: dict):
+    async def create_contact(data: dict):
         data["_id"] = str(uuid.uuid4())
         data["created_at"] = datetime.utcnow()
         await COLLECTION.insert_one(data)
         return data["_id"]
 
     @staticmethod
-    async def get_all_quotes():
+    async def get_all_contacts():
         return await COLLECTION.find().sort("created_at", -1).to_list(None)
 
     @staticmethod
-    async def get_quote_by_id(quote_id: str):
+    async def get_contact_by_id(quote_id: str):
         return await COLLECTION.find_one({"_id": quote_id})
--- app/routes/quote_routes.py
+++ app/routes/quote_routes.py
@@ -11,17 +11,17 @@ router = APIRouter(prefix="/contact-us", tags=["Contact-us"])
 # ---------------- POST (Public) ----------------
 @router.post("/", response_model=dict)
 async def create_quote(payload: QuoteCreate):
-    quote_id = await QuoteRepository.create_quote(payload.dict())
+    quote_id = await QuoteRepository.create_contact(payload.dict())
     return {
-        "message": "Quote request submitted successfully",
+        "message": "Contact-us request submitted successfully",
         "quote_id": quote_id
     }
 
 
 # ---------------- GET ALL (Admin) ----------------
-@router.get("/", dependencies=[Depends(verify_user)])
+@router.get("/")
 async def get_all_quotes():
-    quotes = await QuoteRepository.get_all_quotes()
+    quotes = await QuoteRepository.get_all_contacts()
     return {
         "count": len(quotes),
         "quotes": quotes
@@ -29,9 +29,9 @@ async def get_all_quotes():
 
 
 # ---------------- GET BY ID (Admin) ----------------
-@router.get("/{quote_id}", dependencies=[Depends(verify_user)])
+@router.get("/{quote_id}")
 async def get_quote(quote_id: str):
-    quote = await QuoteRepository.get_quote_by_id(quote_id)
+    quote = await QuoteRepository.get_contact_by_id(quote_id)
     if not quote:
         raise HTTPException(status_code=404, detail="Quote not found")
     return quote
